79306606f4ad3a7537ff9cea5fbbee6d5d5cba4c
Ilya Korzun <64366806+korzunio@users.noreply.github.com>
2021-11-23 07:58:52 +0300

    Обновление адаптации

--- adapt.js
+++ adapt.js
@@ -35,8 +35,6 @@ if (isMobile.any()) {
     document.getElementById("click_block_first").classList.remove('animate__animated');
     document.getElementById("click_block_second").classList.remove('animate__animated');
     document.getElementById("image_fact_first").classList.remove('animate__animated');
-    document.getElementById("image_fact_second").classList.remove('animate__animated');
-    document.getElementById("defenition_promo_archive").classList.remove('animate__animated');
 } else {
     document.body.classList.add('pc');
 }
